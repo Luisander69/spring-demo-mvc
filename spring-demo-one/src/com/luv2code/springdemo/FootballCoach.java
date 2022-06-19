9b4800adbb744082631cfc9d0e56ee85f203e618
lui <stefaniukliubomyr@gmail.cim>
2022-06-19 10:29:58 +0300

    Homework.Added Football coach. Reconfigured xml file

---
+++ spring-demo-one/src/com/luv2code/springdemo/FootballCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Hop the ball 20 times in a row";
+	}
+
+}
