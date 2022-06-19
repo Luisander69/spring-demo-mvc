fd6eb62a4ccf6a6f3d4e6b2d74f39b97ca44a7fa
lui <stefaniukliubomyr@gmail.cim>
2022-06-19 12:19:43 +0300

    Added fortuneService interface. Prepared for dependency injections

--- spring-demo-one/src/com/luv2code/springdemo/FootballCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/FootballCoach.java
@@ -2,10 +2,21 @@ package com.luv2code.springdemo;
 
 public class FootballCoach implements Coach {
 
+private FortuneService fortuneService;
+	
+	public FootballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		
 		return "Hop the ball 20 times in a row";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,11 +1,21 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+private FortuneService fortuneService;
+	
+	public TrackCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
 		return "Run a hard 5k ";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
