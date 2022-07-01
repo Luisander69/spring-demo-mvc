587dd54487d22f0c44bf54fa06777bf9623262ab
lui <stefaniukliubomyr@gmail.cim>
2022-07-01 16:02:32 +0300

    Autowired via Fields

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -5,15 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	@Autowired
 	private FortuneService fortuneService;
 	public TennisCoach() {
 		System.out.println(">>> Inside the default constructor");
 	}
-	@Autowired
-	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println(">>> Inside the setter method");
-		this.fortuneService = fortuneService;
-	}
+	
+	/*
+	 * public void setFortuneService(FortuneService fortuneService) {
+	 * System.out.println(">>> Inside the setter method"); this.fortuneService =
+	 * fortuneService; }
+	 */
 	@Override
 	public String getDailyWorkout() {
 
